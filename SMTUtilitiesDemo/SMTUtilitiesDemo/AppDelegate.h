170a6d6f556535b308ae113f7feb5fde3b017bb0
Steffi <steffi.tan07@gmail.com>
2015-06-27 17:16:29 +0800

    Initial Commits.

---
+++ SMTUtilitiesDemo/SMTUtilitiesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMTUtilitiesDemo
+//
+//  Created by Steffi Tan on 6/27/15.
+//  Copyright (c) 2015 iamsteffi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMTUtilitiesDemo/SMTUtilitiesDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMTUtilitiesDemo
+//
+//  Created by Steffi Tan on 6/27/15.
+//  Copyright (c) 2015 iamsteffi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
