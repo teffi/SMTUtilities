eaea7c47da07944e24896543fd169f055c3c2fa1
Steffi <steffi.tan07@gmail.com>
2015-06-27 17:57:23 +0800

    Added comments/license

--- SMTUtilitiesDemo/SMTUtilitiesDemo/SMTUtilities/SMTUtilities.m
+++ SMTUtilitiesDemo/SMTUtilitiesDemo/SMTUtilities/SMTUtilities.m
@@ -2,8 +2,8 @@
 //  SMTUtilities.m
 //  SMTUtilitiesDemo
 //
-//  Created by Steffi Tan on 6/27/15.
-//  Copyright (c) 2015 iamsteffi.com. All rights reserved.
+//  Created by Teffi : github.com/teffi/SMTUtilities
+//  Copyright (c) 2015 iamsteffi.com
 //
 
 #import "SMTUtilities.h"
